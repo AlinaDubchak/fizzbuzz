9707bf29f1e1a36ef92b6ede321cda11a4783bab
Alina Dubchak <alina.dubchak.ev@gmail.com>
2021-11-11 21:32:23 +0200

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const range = 100;
+console.log('begin');
+for(let i=1; i <= range;i++){
+  if(i%3==0&& i%5===0){
+    console.log('fizzbuzz')}
+  else if(i%5===0){
+    console.log('buzz')
+  }
+  else if(i%3==0){
+    console.log('fizz')}
+  else{
+  console.log(i);
+}
+  }
+console.log('end');
